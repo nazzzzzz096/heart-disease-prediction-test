3e89833cf3e47c3a5c4efb1e60da74eb1f0ba65f
Nazina <nazina096@gmail.com>
2025-11-21 13:10:59 +0530

    change int the api endpoint validataion

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ from fastapi import FastAPI
 from model import predict
 from pydantic import BaseModel
 import numpy as np
+from typing import List
 
 app=FastAPI()
 
 class Dieses(BaseModel):
-    features:list
+    features:List[float]
 
 @app.post('/predict')
 def predict(data:Dieses):
